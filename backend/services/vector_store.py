a14f4d652531b0f71f4a053ff47ba522d45d9d74
Diksha Tiwari <dikshatiwari@Dikshas-MacBook-Air.local>
2026-07-22 11:34:26 +0530

    Use gemini embedding model

--- backend/services/vector_store.py
+++ backend/services/vector_store.py
@@ -2,27 +2,21 @@ import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
-
 class VectorStoreService:
 
     def __init__(self):
-
         self.embeddings = GoogleGenerativeAIEmbeddings(
-            model="models/text-embedding-004",
-            google_api_key=os.getenv("GOOGLE_API_KEY")
+            model="models/gemini-embedding-001",
+            google_api_key=os.getenv("GOOGLE_API_KEY"),
         )
 
-
     def create_and_save_index(self, chunks, doc_id):
-
-        vs = FAISS.from_documents(chunks, self.embeddings)
-        vs.save_local(f"vector_indices/{doc_id}")
-
+        vectorstore = FAISS.from_documents(chunks, self.embeddings)
+        vectorstore.save_local(f"vector_indices/{doc_id}")
 
     def load_index(self, doc_id):
-
         return FAISS.load_local(
             f"vector_indices/{doc_id}",
             self.embeddings,
-            allow_dangerous_deserialization=True
+            allow_dangerous_deserialization=True,
         )
